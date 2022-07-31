1d1dcfb0a743238bee405cf8045f820f4f412305
ArthurPilone <art.pilone@gmail.com>
2022-07-31 12:30:23 -0300

    Add Test Home page
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+const Logo = () => (
+  <Image
+    source={require('./assets/TimerSim.png')}
+    style={{height: '20%', resizeMode: 'contain' }} />
+)
+
+function HomeScreen() {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Logo/>
+        <StatusBar style="auto" />
     </View>
   );
 }
