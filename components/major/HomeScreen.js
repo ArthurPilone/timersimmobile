683ca88a79aca5184c1a2a0efb3a7ed8fc9b7884
ArthurPilone <art.pilone@gmail.com>
2022-12-20 19:33:29 -0300

    Add numeric input component
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- components/major/HomeScreen.js
+++ components/major/HomeScreen.js
@@ -6,6 +6,7 @@ import { Etiqueta } from '../minor/Etiqueta.js';
 import { Botao } from '../minor/Botao.js';
 
 import { ContextoTema } from '../../contextoTema';
+import { InputNumerico } from '../minor/InputNumerico.js';
 
 export { HomeScreen as HomeScreen }
 
@@ -15,12 +16,14 @@ function HomeScreen() {
 			{({estilo, trocaTema}) => (
 				<View style={estilo.container}>
 					<Logo/>
+					<InputNumerico callback={(x) => console.log(x)} step={5} max={60}/>
+					<InputNumerico callback={(x) => console.log(x)} step={1} max={6}/>
 					<Etiqueta h='1' m='30'/>
 					<Etiqueta h='1' m='00'/>
 					<Botao texto='Aperte me' callback={ () => {
 						trocaTema();
 					}} />
-				<StatusBar style="auto" />
+					<StatusBar style="auto" />
 				</View>
 			)}
 		</ContextoTema.Consumer>
---
+++ components/minor/InputNumerico.js
@@ -0,0 +1,19 @@
+import NumericInput from 'react-native-numeric-input'
+
+import { ContextoTema } from '../../contextoTema';
+
+export const InputNumerico = (props) => (
+
+	<ContextoTema.Consumer>
+		{({estilo, trocaTema}) => (
+			<NumericInput
+				onChange={props.callback} 
+				textColor={estilo.texto.color}
+				rightButtonBackgroundColor={estilo.botao.backgroundColor}
+				leftButtonBackgroundColor={estilo.botao.backgroundColor}
+				borderColor={estilo.botao.borderBottomColor}
+				iconStyle={{color:estilo.texto.color}}
+				rounded step={props.step} minValue={0} maxValue={props.max} />
+		)}
+	</ContextoTema.Consumer>
+)  
