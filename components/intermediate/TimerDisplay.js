df87386016c4b8e18257c06f5d54e2258dd38e11
ArthurPilone <art.pilone@gmail.com>
2023-01-16 10:09:48 -0300

    Timer display changes text on test end
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- components/intermediate/TimerDisplay.js
+++ components/intermediate/TimerDisplay.js
@@ -6,11 +6,14 @@ import { Text,View } from 'react-native';
 
 export const TimerDisplay = (props) => {
 
+	let testEnded = (props.s =='00') && (props.ms == '00') && (props.hs == 0)
+
 	return (
 	<ContextoTema.Consumer>
 		{({estilo, trocaTema}) => (
 			<View style={[estilo.card, estilo.timerDisplay]} >
-				<Text style={estilo.subtitulo}>Tempo Restante</Text>
+				{! testEnded && <Text style={estilo.subtitulo}>Tempo Restante</Text>}
+				{testEnded && <Text style={estilo.subtitulo}>Fim de Prova!</Text>}
 				<View style={estilo.rowFlex}>
 					<Text style={estilo.tempoProva}>{props.hs}:{props.ms}:{props.s}</Text>
 				</View>
