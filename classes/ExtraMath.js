bc10d9b7081deae598951a766a47dd2f669b6bbb
ArthurPilone <art.pilone@gmail.com>
2023-01-18 15:33:53 -0300

    Add Library with extra math functions
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

---
+++ classes/ExtraMath.js
@@ -0,0 +1,25 @@
+export {ExtraMath as ExtraMath}
+
+const ExtraMath  = {
+	gaussianRandom,
+	clamp
+}
+
+// The following function was taken from:
+	// https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve, 
+function gaussianRandom(mean, stdev){
+	let u = 1 - Math.random();
+	let v = Math.random();
+	let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+	return z * stdev + mean;
+}
+
+function clamp(val,min,max){
+	if(val < min){
+		return min
+	}else if(val > max){
+		return max
+	}else{
+		return val
+	}
+}
